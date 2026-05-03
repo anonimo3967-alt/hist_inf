7bb8a1193b70600d70f32d520f9e4875c712a980
anonimo3967-alt <anonimo3967@gmail.com>
2026-05-03 14:12:55 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -56,8 +56,8 @@ canvas_result = st_canvas(
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
-    height=500,
-    width=600,
+    height=300,
+    width=400,
     drawing_mode=drawing_mode,
     key="canvas",
 )
